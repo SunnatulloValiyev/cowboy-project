cbda48e3607927d6671030542d1b354952019103
SunnatulloValiyev <sunnatillovaliyev769@gmail.com>
2025-06-17 16:37:30 +0500

    finished

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,23 @@ require("dotenv").config();
 
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PATCH, PUT, DELETE, OPTIONS, HEADER",
+          },
+        ],
+      },
+    ]
+  },
   env: {
     MONGODB_URI: process.env.MONGODB_URI,
   },
